be0a9a88161611323d0ab0342ccf604fb20b4bee
Zeljko Vrba <zeljko.vrba@outlook.com>
2016-02-07 08:01:58 +0100

    Some refactoring. Added test_grid().

--- Solver.h
+++ Solver.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <boost/range/adaptors.hpp>
 #include <boost/signals2.hpp>
 #include "Representation.h"
 
@@ -10,22 +11,23 @@ namespace signals = boost::signals2;
 //! Grid abstraction; contains convenience methods to return rows and columns as ranges.
 class Grid
 {
-  const Description& _description;
-  
 public:
   using Cells = std::vector<int>;
-  Cells cells;
 
-  Grid(const Description& d) :
-    _description(d),
-    cells(_description.width() * _description.height())
+  Grid(size_t w, size_t h) :
+    _width(w), _height(h), _cells(_width * _height)
   { }
 
-  size_t width() const { return _description.width(); }
-  size_t height() const { return _description.height(); }
-  
-  const Line& rowDescription(size_t row) const { return _description.rows.at(row); }
-  const Line& columnDescription(size_t column) const { return _description.columns.at(column); }
+  Cells& cells() { return _cells; }
+  size_t width() const { return _width; }
+  size_t height() const { return _height; }
+
+  int& at(size_t r, size_t c)
+  {
+    if (r >= height() || c >= width())
+      throw std::out_of_range("Grid::at()");
+    return _cells[r*width()+c];
+  }
   
   auto row(size_t row)
   {
@@ -33,7 +35,7 @@ public:
     if (row >= height())
       throw std::invalid_argument("Grid::row()");
     auto b = row * width();
-    return cells | sliced(b, b + width());
+    return _cells | sliced(b, b + width());
   }
   
   auto column(size_t column)
@@ -41,55 +43,36 @@ public:
     using namespace boost::adaptors;
     if (column >= width())
       throw std::invalid_argument("Grid::column()");
-    return cells | sliced(column, cells.size()) | strided(width());
+    return _cells | sliced(column, _cells.size()) | strided(width());
+  }
+  
+private:
+  size_t _width, _height;
+  Cells _cells;
+};
+
+/////////////////////////////////////////////////////////////////////////////
+
+
+struct and_combiner
+{
+  typedef bool result_type;
+
+  template<typename It>
+  bool operator()(It begin, It end) const
+  {
+    while (begin != end)
+    if (!*begin++)
+      return false;
+    return true;
   }
 };
 
-/////////////////////////////////////////////////////////////////////////////
+using LineChangedSignal = signals::signal_type<
+        bool(size_t),
+        signals::keywords::combiner_type<and_combiner>,
+        signals::keywords::mutex_type<signals::dummy_mutex>>::type;
 
-//! Checks whether the current state of the board satisfies the column description.
-class ColumnAgent
-{
-  Grid& _grid;
-  size_t _column;
-  
-public:
-  ColumnAgent(Grid& grid, size_t column) : _grid(grid), _column(column)
-  { }
-  bool operator()(size_t column) const;
-};
-
-/////////////////////////////////////////////////////////////////////////////
-
-class RowAgent
-{
-  Grid& _grid;
-  LineVector _validLines;
-  
-  struct and_combiner
-  {
-    typedef bool result_type;
-
-    template<typename It>
-    bool operator()(It begin, It end) const
-    {
-      while (begin != end)
-      if (!*begin++)
-        return false;
-      return true;
-    }
-  };
-  
-public:
-  using signal_type = signals::signal_type<
-          bool(size_t),
-          signals::keywords::combiner_type<and_combiner>,
-          signals::keywords::mutex_type<signals::dummy_mutex>>::type;
-  
-  RowAgent(Grid& grid, const LineVector& lv);
-  void next();
-  signals::signal<bool(size_t), and_combiner> columnChanged;
-};
 
 
 }
--- main.cpp
+++ main.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 #include <fstream>
 #include "Representation.h"
+#include "Solver.h"
 
-int main(int argc, char** argv)
+using namespace nonogram;
+
+static void test_enumerate()
 {
-  using namespace nonogram;
-  
   Line line{1,1};
   auto e = enumerateLine(line, 5, 0);
   
@@ -13,7 +14,39 @@ int main(int argc, char** argv)
     for (auto v : l) std::cout << v << ' ';
     std::cout << '\n';
   }
+}
+
+static void test_grid()
+{
+  using std::cout;
+  using std::endl;
   
+  Grid g(5,7);
+  
+  for (int i = 0; i < 35; ++i)
+    g.cells()[i] = i;
+  
+  cout << "ROWS:\n";
+  for (int r = 0; r < g.height(); ++r) {
+    auto row = g.row(r);
+    row[0] += 10;
+    for (auto v: row) cout << v << " ";
+    cout << endl;
+  }
+
+  cout << "\nCOLUMNS:\n";
+  for (int c = 0; c < g.width(); ++c) {
+    auto col = g.column(c);
+    col[1] += 11;
+    for (auto v: col) cout << v << " ";
+    cout << endl;
+  }
+}
+
+int main(int argc, char** argv)
+{
+  test_grid();
   return 0;
 }
+
 
