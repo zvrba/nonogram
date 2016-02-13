19d9ff7adc1860d3b36b4770721eb588e7bea39f
Zeljko Vrba <zeljko.vrba@outlook.com>
2016-02-13 09:49:49 +0100

    Added pretty-printing of solution.

--- main.cpp
+++ main.cpp
@@ -1,17 +1,23 @@
 #include <iostream>
 #include <fstream>
-#include <fstream>
 #include "Solver.h"
 
 using namespace nonogram;
+using std::cout;
 
-static void test_enumerate()
+static void print_solution(const Solver::Solution& solution)
 {
-  LineDescription line{2,1};
-  auto e = enumerateColorings(line, 5);
-  
-  for (const auto& l : e) {
-    std::cout << l << '\n';
+  for (const LineColoring& line : solution) {
+    size_t column = 0;
+    for (const Block& block : line) {
+      const auto blockBegin = std::get<0>(block);
+      const auto blockEnd = std::get<1>(block);
+      for (; column < blockBegin; ++column)
+        cout << '.';
+      for (; column < blockEnd; ++column)
+        cout << '#';
+    }
+    cout << '\n';
   }
 }
 
@@ -32,7 +38,12 @@ int main(int argc, char** argv)
     std::vector<Solver::Solution> solutions;
     Solver solver(d, solutions);
     solver.solve();
-    std::cout << "found " << solutions.size() << " solutions\n";
+    
+    cout << "FOUND " << solutions.size() << " SOLUTIONS\n";
+    for(size_t i = 0; i < solutions.size(); ++i) {
+      cout << "\n\nSOLUTION " << i << '\n';
+      print_solution(solutions[i]);
+    }
   } catch (std::exception& err) {
     std::cerr << "ERROR: " << err.what() << '\n';
     return 1;
