1ca3170b4ec0031e248195f4e8bd64d430d234c3
Zeljko Vrba <zeljko.vrba@outlook.com>
2016-02-13 09:20:40 +0100

    Removed unused class and test code.

--- Solver.h
+++ Solver.h
@@ -5,52 +5,6 @@
 namespace nonogram
 {
 
-//! Grid abstraction; contains convenience methods to return rows and columns as
-//! mutable random-access ranges.
-class Grid
-{
-public:
-  using Cells = std::vector<int>;
-
-  Grid(size_t w, size_t h) :
-    _width(w), _height(h), _cells(_width * _height)
-  { }
-
-  Cells& cells() { return _cells; }
-  size_t width() const { return _width; }
-  size_t height() const { return _height; }
-
-  int& at(size_t r, size_t c)
-  {
-    if (r >= height() || c >= width())
-      throw std::out_of_range("Grid::at()");
-    return _cells[r*width()+c];
-  }
-  
-  auto row(size_t row)
-  {
-    using namespace boost::adaptors;
-    if (row >= height())
-      throw std::invalid_argument("Grid::row()");
-    auto b = row * width();
-    return _cells | sliced(b, b + width());
-  }
-  
-  auto column(size_t column)
-  {
-    using namespace boost::adaptors;
-    if (column >= width())
-      throw std::invalid_argument("Grid::column()");
-    return _cells | sliced(column, _cells.size()) | strided(width());
-  }
-  
-private:
-  size_t _width, _height;
-  Cells _cells;
-};
-
-/////////////////////////////////////////////////////////////////////////////
-
 //! Verifies whether RowAgent's placement of the cell is consistent with column description.
 class ColumnAgent
 {
